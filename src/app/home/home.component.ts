0c3374ec98fba0b60fede3b34a8e5452d2e9e138
Alex <alexardeleanu2001@gmail.com>
2024-11-20 23:19:28 +0000

    -move background on hero-body tag -add hom button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,15 +12,17 @@ import { Component, OnInit } from '@angular/core';
       Ihrer Hobbywerkstatt in Fürth!</p>
       <p class="has-text-primary-light has-text-weight-semibold is-size-6">Die Hobbywerkstatt in Fürth, Tuner's Edge ist seit Februar 2016 für Sie im Dienst. <br>
       Wir werden Sie mit moderner Ausstattung erwarten. <br> Schauen Sie sich unsere neuen Räumlichkeiten an und lassen <br> Sie uns bei einem Kaffee über die Vorteile der selbständigen Reparatur des eigenen Autos sprechen.</p>
+      <div class="block"></div>
+      <button class="button is-primary">View Premisses</button>
     </div>
   </div>
 </section>
   `,
   styles: [`
-  .hero{ 
+  .hero-body{ 
     background-image: url('src/assets/img/home-wallpaper.jpeg') !important;
     background-size: cover;
- 
+    background-position: center center;
   }`]
 })
 export class HomeComponent {

0daefa87903f2d309cef2e9049d3fdace6a3cf4c
Alex <alexardeleanu2001@gmail.com>
2025-01-27 20:27:16 +0000

    merge unique html components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,37 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { FooterComponent } from "./footer/footer.component";
-import { HomeComponent } from "./home/home.component";
-import { ServicesComponent } from "./services/services.component";
-import { AboutComponent } from "./about/about.component";
-import { ReviewsComponent } from "./reviews/reviews.component";
-import { ContactsComponent } from "./contacts/contacts.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent, HomeComponent, ServicesComponent, AboutComponent, ReviewsComponent, ContactsComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   template: `
     <!-- nav -->
     <app-nav/>
 
-    <!-- home -->
-    <app-home/>
-
-    <!-- services -->
-    <app-services/>
-
     <!-- routes get injected here -->
     <router-outlet/>
-
-    <!-- about -->
-    <app-about/>
-
-    <!-- reviews -->
-    <app-reviews/>
-
-    <!-- contacts -->
-    <app-contacts/>
 
     <!-- footer -->
     <app-footer/>

ef6cdda35d61403cc08e69a62edc6795eac238c4
Alex <alexardeleanu2001@gmail.com>
2025-02-01 13:27:59 +0000

    - nav logo html

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -5,35 +5,35 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-<nav class="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
-<div class="container">
-  <div class="navbar-brand">
-    <a class="navbar-item has-text-weight-bold has-text-light is-size-5" href="#home">TUNERSEDGE</a>
+  <nav class="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item has-text-weight-bold has-text-light is-size-5" href="#home">TUNERSEDGE</a>
 
-    <div class="navbar-logo">
-      <a href="#home" class="logo-link">
-        <div class="logo"></div>
+      <div class="navbar-logo">
+        <a href="#home" class="logo-link">
+          <div class="logo"></div>
+        </a>
+      </div>
+
+      <a (click)="toggleNavbar()" #navBurger role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+        <span class="has-text-light" aria-hidden="true"></span>
+        <span class="has-text-light" aria-hidden="true"></span>
+        <span class="has-text-light" aria-hidden="true"></span>
+        <span class="has-text-light" aria-hidden="true"></span>
       </a>
     </div>
 
-    <a (click)="toggleNavbar()" #navBurger role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
-      <span class="has-text-light" aria-hidden="true"></span>
-      <span class="has-text-light" aria-hidden="true"></span>
-      <span class="has-text-light" aria-hidden="true"></span>
-      <span class="has-text-light" aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div #navMenu id="navMenu" class="navbar-menu is-size-6">
-    <div class="navbar-end has-text-weight-medium">
-      <a class="navbar-item has-text-light" href="#home">Home</a>
-      <a class="navbar-item has-text-light" href="#services">Services</a>
-      <a class="navbar-item has-text-light" href="#about">About</a>
-      <a class="navbar-item has-text-light" href="#contacts">Contacts</a>
+    <div #navMenu id="navMenu" class="navbar-menu is-size-6">
+      <div class="navbar-end has-text-weight-medium">
+        <a class="navbar-item has-text-light" href="#home">Home</a>
+        <a class="navbar-item has-text-light" href="#services">Services</a>
+        <a class="navbar-item has-text-light" href="#about">About</a>
+        <a class="navbar-item has-text-light" href="#contacts">Contacts</a>
+      </div>
     </div>
   </div>
-</div>
-</nav>
+  </nav>
   `,
   styles: ``
 })
